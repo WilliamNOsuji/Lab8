80532bcde2636c3cae07d423388e8d5ca5c23100
WilliamOsuji <2079555@cegepmontpetit.ca>
2024-03-22 23:27:05 -0400

    done

--- Lab8/Data/AnimalService.cs
+++ Lab8/Data/AnimalService.cs
@@ -14,68 +14,9 @@ namespace Lab8.Data
             _context = context;
         }
 
-        private bool IsContextNull() 
-        { return _context != null || _context.Animal == null; }
-
-        public async Task<IEnumerable<Animal>?> GetAll()
-        {
-            if (IsContextNull()) return null;
-            
-            return await _context.Animal.ToListAsync();
-        }
-
-        public async Task<Animal?> Get(int id)
-        {
-            if (IsContextNull()) return null;
-
-            return await _context.Animal.FindAsync(id);
-        }
-
-        public async Task<Animal?> Add(Animal animal)
-        {
-            if(IsContextNull()) return null;
-
-            _context.Animal.Add(animal);
-            await _context.SaveChangesAsync();
-            return animal;
-        }
-
-        public async Task<Animal?> Delete(int id)
-        {
-            if (IsContextNull()) return null;
-
-            Animal? animal = await Get(id);
-
-            if (animal == null) return null;
-
-            _context.Animal.Remove(animal);
-
-            await _context.SaveChangesAsync();
-
-            return animal;
-        }
-
-        public async Task<Animal?> Edit(int id, Animal animal)
-        {
-            if (IsContextNull()) return null;
-
-            _context.Entry(animal).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if ((await Get(id)) == null) return null;
-                else throw;
-            }
-            return animal;
-        }
-
         public async Task ClearAllAnimals()
         {
-            if (IsContextNull()) return;
+            if (_context == null) return;
 
             _context.Animal.RemoveRange(_context.Animal);
 
--- Lab8/Data/GenericService.cs
+++ Lab8/Data/GenericService.cs
@@ -13,23 +13,23 @@ namespace Lab8.Data
         }
 
         private bool IsContextNull()
-        { return _context != null || _context.Set<T>() == null; }
+        { return _context == null || _context.Set<T>() == null; }
 
-        public async Task<IEnumerable<T>?> GetAll()
+        public virtual async Task<IEnumerable<T>?> GetAll()
         {
             if (IsContextNull()) return null;
 
             return await _context.Set<T>().ToListAsync();
         }
 
-        public async Task<T?> Get(int id)
+        public virtual async Task<T?> Get(int id)
         {
             if (IsContextNull()) return null;
 
             return await _context.Set<T>().FindAsync(id);
         }
 
-        public async Task<T?> Add(T t)
+        public virtual async Task<T?> Add(T t)
         {
             if (IsContextNull()) return null;
 
@@ -38,11 +38,11 @@ namespace Lab8.Data
             return t;
         }
 
-        public async Task<T?> Delete(int id)
+        public virtual async Task<T?> Delete(int id)
         {
             if (IsContextNull()) return null;
 
-            var t = await Get(id);
+            var t = await _context.Set<T>().FindAsync(id);
 
             if (t == null) return null;
 
@@ -53,10 +53,8 @@ namespace Lab8.Data
             return t;
         }
 
-        public async Task<T?> Edit(int id, T t)
+        public virtual async Task<T?> Edit(int id, T t)
         {
-            if (IsContextNull()) return null;
-
             _context.Entry(t).State = EntityState.Modified;
 
             try
@@ -65,10 +63,19 @@ namespace Lab8.Data
             }
             catch (DbUpdateConcurrencyException)
             {
-                if ((await Get(id)) == null) return null;
+                if ((await _context.Set<T>().FindAsync(id)) == null) return null;
                 else throw;
             }
             return t;
+        }
+
+        public virtual async Task DeleteAll()
+        {
+            if (IsContextNull()) return;
+
+            _context.Set<T>().RemoveRange(await _context.Set<T>().ToListAsync());
+
+            await _context.SaveChangesAsync();
         }
     }
 }
